430984992392f83aca015e30861bcde163be0d6e
LG972021 <liamgill97@hotmail.com>
2021-08-16 22:15:24 +0100

    Correct Add Function Test

--- addFunction.test.js
+++ addFunction.test.js
@@ -1,1 +1,18 @@
 const adding = require ('./addFunction');
+
+describe ('adding', () => {
+test ('Test if 2 numbers are added together correctly', () => {
+    let actualOutput = adding(2,5);
+    let expectedOutput = 7;
+    expect(actualOutput).toEqual(expectedOutput)
+
+    actualOutput = adding(4,4);
+    expectedOutput = 8;
+    expect(actualOutput).toEqual(expectedOutput)
+
+    actualOutput = adding(4,2);
+    expectedOutput = 6;
+    expect(actualOutput).toEqual(expectedOutput)
+
+    });
+});
